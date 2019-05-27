3d9d382f6610af3ff5fe79bb96eee694f9ed0bda
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-27 23:57:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,13 +6,13 @@ class App < Sinatra::Base
   end
 
   post '/newteam' do
-    params[:name]
-    params[:coach]
-    params[:pg]
-    params[:sg]
-    params[:sf]
-    params[:pf]
-    params[:c]
+    # params[:name]
+    # params[:coach]
+    # params[:pg]
+    # params[:sg]
+    # params[:sf]
+    # params[:pf]
+    # params[:c]
 
     @all = "#{params[:name]}" "#{params[:coach]}" "#{params[:pg]}" "#{params[:sg]}" "#{params[:sf]}" "#{params[:pf]}" "#{params[:c]}"
     erb :team
