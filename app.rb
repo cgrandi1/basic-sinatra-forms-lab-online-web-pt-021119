eb04c2161b9c0b7ec81b7a99b233a62482c082ea
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-27 23:40:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,11 +2,12 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
   get '/newteam' do
-
     erb :newteam
   end
 
   post '/team' do
+    Teamname = params["text"]
+    Coach = params[:Coach]
     erb :team
   end
 
